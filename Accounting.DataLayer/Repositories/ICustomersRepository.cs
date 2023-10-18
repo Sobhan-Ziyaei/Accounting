e92ea8aae4463c6951ed9ad7b3ede02193a5174c
Sobhan-Ziyaei <sobhan.ziaee73@gmail.com>
2023-10-18 08:51:20 +0330

    create interface of cusomer in repository

---
+++ Accounting.DataLayer/Repositories/ICustomersRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repositories
+{
+    public interface ICustomersRepository
+    {
+        List<Customers> getAllCustomers();
+        Customers getCustomerById(int customerId);
+        bool insertCustomer(Customers customer);
+        bool updateCustomer(Customers customer);
+        bool deleteCustomer(Customers customer);
+        bool deleteCustomer(int customerId);
+        void save();
+    }
+}

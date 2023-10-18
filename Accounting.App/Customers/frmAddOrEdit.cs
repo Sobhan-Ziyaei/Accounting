8757588eb75b52b0c90a2240f4bc8fb355a53449
Sobhan-Ziyaei <sobhan.ziaee73@gmail.com>
2023-10-18 13:48:16 +0330

    create insert customer

---
+++ Accounting.App/Customers/frmAddOrEdit.cs
@@ -0,0 +1,66 @@
+﻿using Accounting.DataLayer;
+using Accounting.DataLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ValidationComponents;
+
+namespace Accounting.App
+{
+    public partial class frmAddOrEdit : Form
+    {
+        UnitOfWork db = new UnitOfWork();
+        public frmAddOrEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelectPhoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pcCustomer.ImageLocation = openFile.FileName;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (BaseValidator.IsFormValid(this.components))
+            {
+                string imageName = Guid.NewGuid().ToString() + Path.GetExtension(pcCustomer.ImageLocation);
+                string path = Application.StartupPath + "/Images";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                pcCustomer.Image.Save(path+imageName);
+                Customers customer = new Customers()
+                {
+
+                    FullName = txtName.Text,
+                    Mobile = txtMobile.Text,
+                    Email = txtEmail.Text,
+                    Address = txtAddress.Text,
+                    CustomerImage = imageName,
+
+                };
+                db.customersRepository.insertCustomer(customer);
+                db.save();
+                DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
---
+++ Accounting.App/Customers/frmCustomers.cs
@@ -0,0 +1,91 @@
+﻿using Accounting.DataLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accounting.App
+{
+    public partial class frmCustomers : Form
+    {
+        public frmCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomers_Load(object sender, EventArgs e)
+        {
+            dgCustomers.AutoGenerateColumns = false;
+            BindGrid();
+        }
+
+        void BindGrid()
+        {
+
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                dgCustomers.DataSource = db.customersRepository.getAllCustomers();
+            }
+        }
+
+        private void btnRefreshCustomer_Click(object sender, EventArgs e)
+        {
+            txtFilterCustomer.Text = "";
+            BindGrid();
+        }
+
+        private void txtFilterCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFilterCustomer_TextChanged(object sender, EventArgs e)
+        {
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                dgCustomers.DataSource = db.customersRepository.getCustomersByFilter(txtFilterCustomer.Text);
+
+            }
+        }
+
+        private void btnDeleteCustomer_Click(object sender, EventArgs e)
+        {
+            if (dgCustomers.CurrentRow != null)
+            {
+                using (UnitOfWork db = new UnitOfWork())
+                {
+                    string name = dgCustomers.CurrentRow.Cells[1].Value.ToString();
+                    if (RtlMessageBox.Show($"آیا از حذف {name} مطمئن هستید ؟", "توجه", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    {
+                        int customerId = int.Parse(dgCustomers.CurrentRow.Cells[0].Value.ToString());
+                        db.customersRepository.deleteCustomer(customerId);
+                        db.save();
+                        BindGrid();
+                    }
+                }
+            }
+            else
+            {
+                RtlMessageBox.Show("لطفا یک شخص را انتخاب کنید");
+            }
+        }
+
+        private void btnAddNewCustomer_Click(object sender, EventArgs e)
+        {
+            frmAddOrEdit frmAddOrEdit = new frmAddOrEdit();
+            if (frmAddOrEdit.ShowDialog() == DialogResult.OK)
+            {
+                BindGrid();
+            }
+
+
+        }
+
+
+    }
+}
